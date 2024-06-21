254764d885c920306ab383b62b55cfe2850ca650
amolpradeshi <pardeshiharshal90@gmail.com>
2024-06-21 14:09:44 +0530

    fixed json-server-links

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -2,42 +2,37 @@ import axios from "axios";
 
 export const GetLoginData = (email, password, number) => {
   return axios.get(
-    `https://jsonmockserver.cyclic.app/user?number=${number}&email=${email}&password=${password}`
+    `https://jsonmockserver.onrender.com/user?number=${number}&email=${email}&password=${password}`
   );
 };
 
 export const PostLoginData = async ({ name, email, number, password }) => {
-  await fetch(
-    "https://jsonmockserver.cyclic.app/user",
-    {
-      method: "POST",
-      body: JSON.stringify({ name, email, number, password }),
-      headers: { "Content-Type": "application/json" },
-    }
-  );
+  await fetch("https://jsonmockserver.onrender.com/user", {
+    method: "POST",
+    body: JSON.stringify({ name, email, number, password }),
+    headers: { "Content-Type": "application/json" },
+  });
 };
 
 export const GetCatData = () => {
-  return axios.get(
-    "https://jsonmockserver.cyclic.app/nbCategory"
-  );
+  return axios.get("https://jsonmockserver.onrender.com/nbCategory");
 };
 
 export const GetWholeData = (setData) => {
   axios
-    .get("https://jsonmockserver.cyclic.app/nbFruits&Veg")
+    .get("https://jsonmockserver.onrender.com/nbFruits&Veg")
     .then((res) => res.json());
 };
 
 export const GetCartData = (setData) => {
   axios
-    .get("https://jsonmockserver.cyclic.app/nbCart")
+    .get("https://jsonmockserver.onrender.com/nbCart")
     .then((res) => setData(res.data));
 };
 
 export const CartDataAdd = (data) => {
   axios({
-    url: `https://jsonmockserver.cyclic.app/nbCart`,
+    url: `https://jsonmockserver.onrender.com/nbCart`,
     method: "POST",
     data: data,
   });
@@ -45,14 +40,14 @@ export const CartDataAdd = (data) => {
 
 export const CartItemDelete = (id) => {
   axios({
-    url: `https://jsonmockserver.cyclic.app/nbCart/${id}`,
+    url: `https://jsonmockserver.onrender.com/nbCart/${id}`,
     method: "DELETE",
   });
 };
 
 export const CountIncDec = (id, no, qty, price, aPrice) => {
   axios({
-    url: `https://jsonmockserver.cyclic.app/nbCart/${id}`,
+    url: `https://jsonmockserver.onrender.com/nbCart/${id}`,
     method: "PATCH",
     data: { qty: Number(qty) + no, price: Number(price) + Number(aPrice) * no },
   });
